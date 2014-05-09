ad3033be95515a622a63ce4e55669e119101525b
Todd S <starco58@gmail.com>
2014-05-08 22:02:48 -0500

    Early steps...
    not much progress yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "calculations", :action => "home" })
+
+  get("/calculations", { :controller => "calculations", :action => "home" })
   get("/square_root/:number", { :controller => "calculations", :action => "sqrt" })
   get("/payment/:interest_rate/:number_of_payments/:present_value", { :controller => "calculations", :action => "pmt" })
 end
