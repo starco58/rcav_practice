4c224508c23b9b7d3cc96caa6b7bf603f3db5a5c
Todd S <starco58@gmail.com>
2014-05-10 12:55:04 -0500

    all routes work
    URLs work, need to figure out next step

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   get("/calculations",        { :controller => "calculations", :action => "home" })
   get("/calculations/:sqrt",  { :controller => "calculations", :action => "sqrt" })
-  get("/payment/:interest_rate/:number_of_payments/:present_value", { :controller => "calculations", :action => "pmt" })
+  get("/calculations/:payment/:interest_rate/:number_of_payments/:present_value", { :controller => "calculations", :action => "pmt" })
 end
