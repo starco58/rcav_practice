419a9633749f8a0a016f219b6b2ca74f2224e962
Todd S <starco58@gmail.com>
2014-05-12 18:02:25 -0500

    final corrections

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -3,20 +3,25 @@ class CalculationsController < ApplicationController
 
   end
 
+  def square
+    @your_number = params[:number].to_f
+    @square_of_the_number = (@your_number)**2
+  end
+
   def sqrt
     @the_number = params[:number].to_f
     @answer = Math.sqrt(@the_number)
   end
 
   def pmt
-    @interest_rate = params[:interest_rate].to_i
-    @number_of_payments = params[:number_of_payments].to_i
-    @present_value = params[:present_value].to_i
+    @interest_rate = params[:interest_rate].to_f
+    @number_of_payments = params[:number_of_payments].to_f
+    @present_value = params[:present_value].to_f
     rate12 = @interest_rate/100/12
     numerator = @present_value*rate12.to_f
     denomenator = 1-(1+rate12.to_f)**-@number_of_payments
 
-    @payment = numerator/denomenator.to_f # "#{(@interest_rate,@number_of_payments,@present_value)}"
+    @payment = (numerator/denomenator).to_i #"#{(@interest_rate,@number_of_payments,@present_value)}"
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get("/calculations",        { :controller => "calculations", :action => "home" })
-  get("/calculations/square/:sqrt",  { :controller => "calculations", :action => "sqrt" })
-  get("/calculations/:payment/:interest_rate/:number_of_payments/:present_value", { :controller => "calculations", :action => "pmt" })
+  get("/square_root/:number",  { :controller => "calculations", :action => "sqrt" })
+  get("/square/:number",  { :controller => "calculations", :action => "square" })
+  get("/payment/:interest_rate/:number_of_payments/:present_value", { :controller => "calculations", :action => "pmt" })
 end
