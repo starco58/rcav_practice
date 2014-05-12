9321dc33d3d0cb1f5a21126d1bbcb000530db915
Todd S <starco58@gmail.com>
2014-05-12 09:43:44 -0500

    trying to get payment to show
    playing with code shared on conversation

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -9,6 +9,14 @@ class CalculationsController < ApplicationController
   end
 
   def pmt
+    @interest_rate = params[:interest_rate].to_i
+    @number_of_payments = params[:number_of_payments].to_i
+    @present_value = params[:present_value].to_i
+    rate12 = @interest_rate/100/12
+    numerator = @present_value*rate12.to_f
+    denomenator = 1-(1+rate12.to_f)**-@number_of_payments
+
+    @payment = numerator/denomenator.to_f # "#{(@interest_rate,@number_of_payments,@present_value)}"
 
   end
 end
