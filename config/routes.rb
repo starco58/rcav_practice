4b980729b845512b7ce915c1978edfcb7ced611e
Todd S <starco58@gmail.com>
2014-05-12 08:51:43 -0500

    Working on square root mat
    URL works, but giving me 0.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   get("/calculations",        { :controller => "calculations", :action => "home" })
-  get("/calculations/:sqrt",  { :controller => "calculations", :action => "sqrt" })
+  get("/calculations/square/:sqrt",  { :controller => "calculations", :action => "sqrt" })
   get("/calculations/:payment/:interest_rate/:number_of_payments/:present_value", { :controller => "calculations", :action => "pmt" })
 end
